dc25a32d517dd78d898824342508351122a4da88
TomasLey <113106085+TomasLey@users.noreply.github.com>
2022-12-15 10:28:23 +0100

    Update Decorators.py

--- Decorators/Decorators.py
+++ Decorators/Decorators.py
@@ -17,26 +17,26 @@ def decorator_octava(melody_ly):     # Coge nota p.e. a4 y saca <a4 a4'>
             if bar[i] != " ":                   # detecta el primer caracter que no es un espacio
                 j = bar.find(" ",i,len(bar))    # CALCULAR posicion del siguiente espacio
                 first_note = bar[i:j]           # primera nota = cosas entre 2 espacios p.e cis'4
-                
-                for m in first_note:  
-                    if m == "'":
-                        mPos = first_note.find("'")
-                        octava = "<" + first_note[0:mPos+1] + " " + first_note[0:mPos+1] + " " + ">" + first_note[mPos+1:len(first_note)]
-                        break
-                    elif m == ",": 
-                        mPos = first_note.find(",")
-                        octava = "<" + first_note[0:mPos+1] + " " + first_note[0:mPos]  + ">" + first_note[mPos+1:len(first_note)]
-                        break
-                    elif m.isdigit():
-                        mPos = first_note.find(m)
-                        first_note_clean = first_note[0:len(first_note)-1]
-                        octava = "<" + first_note_clean[0:mPos] + " " + first_note_clean[0:mPos] + "'"  + ">" + first_note[mPos:len(first_note)]
-                        break
-                    else:
-                        if m == first_note[-1]:
-                            octava = "<" + first_note + " " +  first_note[0] + "'" + ">"
-                            break       # termina el bucle
-                break
+                if first_note[0] != r:
+                   for m in first_note:  
+                       if m == "'":
+                           mPos = first_note.find("'")
+                           octava = "<" + first_note[0:mPos+1] + " " + first_note[0:mPos+1] + " " + ">" + first_note[mPos+1:len(first_note)]
+                           break
+                       elif m == ",": 
+                           mPos = first_note.find(",")
+                           octava = "<" + first_note[0:mPos+1] + " " + first_note[0:mPos]  + ">" + first_note[mPos+1:len(first_note)]
+                           break
+                       elif m.isdigit():
+                           mPos = first_note.find(m)
+                           first_note_clean = first_note[0:len(first_note)-1]
+                           octava = "<" + first_note_clean[0:mPos] + " " + first_note_clean[0:mPos] + "'"  + ">" + first_note[mPos:len(first_note)]
+                           break
+                       else:
+                           if m == first_note[-1]:
+                               octava = "<" + first_note + " " +  first_note[0] + "'" + ">"
+                               break       # termina el bucle
+                   break
         melody_ly_decorator[n] = "  " + octava + bar[j:len(bar)]
 
     return(melody_ly_decorator) 
